bc8bc4c5fdb92e38aa9235e3591e4ec11b26a64e
rakgit-hub <83184805+rakgit-hub@users.noreply.github.com>
2021-05-10 15:22:25 -0400

    Delete SpeakHello.js

--- SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-	var speakWord = "Hello";
-	var name = "Dhanvin";
-	//var helloSpeaker = function speak(name) {
-		console.log(speakWord + " " + name);
-	//}
-	//window.helloSpeaker = helloSpeaker;
-})(window);
