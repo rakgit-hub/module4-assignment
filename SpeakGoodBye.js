179a647d23c39dfab4dd521e2631ae93cf24034c
rakgit-hub <83184805+rakgit-hub@users.noreply.github.com>
2021-05-10 15:22:19 -0400

    Delete SpeakGoodBye.js

--- SpeakGoodBye.js
+++
@@ -1,7 +0,0 @@
-(function(window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = function speak (name) {
-		console.log(speakWord + " " + name);
-	}
-	window.byeSpeaker = byeSpeaker;
-})(window);
